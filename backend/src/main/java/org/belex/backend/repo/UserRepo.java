9382025ea7828fac2d03948298be737e5a0a890f
Arbaj Ali <thewrongturn6show@gmail.com>
2026-09-21 14:10:10 +0530

    created user repo

---
+++ backend/src/main/java/org/belex/backend/repo/UserRepo.java
@@ -0,0 +1,11 @@
+package org.belex.backend.repo;
+
+import org.belex.backend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepo extends JpaRepository<User,Long> {
+    Optional<User> findByEmailIgnoreCase(String email);
+    boolean existsByEmailIgnoreCase(String email);
+}
