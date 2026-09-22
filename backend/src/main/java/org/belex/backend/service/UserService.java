ae38c1fb3a081d2e4f9303451c470e177ef9114d
Arbaj Ali <thewrongturn6show@gmail.com>
2026-09-22 11:19:34 +0530

    UserService completely implemented

--- backend/src/main/java/org/belex/backend/service/UserService.java
+++ backend/src/main/java/org/belex/backend/service/UserService.java
@@ -1,6 +1,37 @@
 package org.belex.backend.service;
 
+import org.belex.backend.model.User;
+import org.belex.backend.repo.UserRepo;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
 public class UserService implements UserDetailsService {
+    private final UserRepo userRepo;
+    public UserService(UserRepo userRepo) {
+        this.userRepo = userRepo;
+    }
+
+    public User getRequired(String email){
+        return userRepo.findByEmailIgnoreCase(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+
+    @Override
+    public UserDetails  loadUserByUsername(String email){
+        User user = getRequired(email);
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(), user.getPassword(),
+                List.of(new SimpleGrantedAuthority("ROLE_USER"))
+        );
+    }
+
+    public User getFromPrincipal(UserDetails principal){
+        return getRequired(principal.getUsername());
+    }
 }
